f1570b75bf85ed051b9343f77a2a5a0a6ff81851
Kirill <dremkiri@gmail.com>
2022-01-29 02:20:21 +0200

    1

---
+++ main.js
@@ -0,0 +1,271 @@
+// if 1
+// let a = +prompt('Enter your number')
+// if(a > 0){
+//     a = a + 1;
+//     alert(a)
+//     console.log(a)
+// } else{
+//     alert(a)
+//     console.log(a)
+// }
+
+
+
+// if 2
+// let a = +prompt('Enter your number')
+// if(a > 0){
+//     a = a + 1;
+//     alert(a)
+//     console.log(a)
+// } else{
+//     a = a - 2; 
+//     alert(a)
+//     console.log(a)
+// }
+
+
+
+// if 3
+// let a = +prompt('Enter your number')
+// if(a > 0){
+//     a = a + 1;
+//     alert(a)
+//     console.log(a)
+// } else if(a < 0){
+//     a = a - 2; 
+//     alert(a)
+//     console.log(a)
+// } else{
+//     a = 10;
+//     alert(a)
+//     console.log(a)
+// }
+
+
+
+// if 4
+// let a = +prompt('Enter first number')
+// let b = +prompt('Enter second number')
+// let c = +prompt('Enter third number')
+// if(a > 0 && b > 0 && c > 0){
+//     alert('Three numbers')
+// } else if(a > 0 && b > 0 && c < 0){
+//     alert('Two numbers')
+// } else if(a > 0 && b < 0 && c > 0){
+//     alert('Two numbers')
+// } else if(a < 0 && b > 0 && c > 0){
+//     alert('Two numbers')
+// } else if(a > 0 && b < 0 && c < 0){
+//     alert('One number')
+// } else if(a < 0 && b > 0 && c < 0){
+//     alert('One number')
+// } else if(a < 0 && b < 0 && c > 0){
+//     alert('One number')
+// } else if(a < 0 && b < 0 && c < 0){
+//     alert('Zero numbers')
+// }
+
+
+
+// if 5
+// let a = +prompt('Enter first number')
+// let b = +prompt('Enter second number')
+// let c = +prompt('Enter third number')
+// if(a > 0 && b > 0 && c > 0){
+//     alert('Three numbers > 0')
+//     alert('Zero numbers < 0')
+// } else if(a > 0 && b > 0 && c < 0){
+//     alert('Two numbers > 0')
+//     alert('One number < 0')
+// } else if(a > 0 && b < 0 && c > 0){
+//     alert('Two numbers > 0')
+//     alert('One number < 0')
+// } else if(a < 0 && b > 0 && c > 0){
+//     alert('Two numbers > 0')
+//     alert('One number < 0')
+// } else if(a > 0 && b < 0 && c < 0){
+//     alert('One number > 0')
+//     alert('Two numbers < 0')
+// } else if(a < 0 && b > 0 && c < 0){
+//     alert('One number > 0')
+//     alert('Two numbers < 0')
+// } else if(a < 0 && b < 0 && c > 0){
+//     alert('One number > 0')
+//     alert('Two numbers < 0')
+// } else if(a < 0 && b < 0 && c < 0){
+//     alert('Zero numbers > 0')
+//     alert('Three numbers < 0')
+// }
+
+
+
+// if 6
+// let a = +prompt('Enter first number')
+// let b = +prompt('Enter second number')
+// if(a > b){
+//     alert(a)
+// } else{
+//     alert(b)
+// }
+
+
+
+// if 7
+// let a = +prompt('Enter first number')
+// let b = +prompt('Enter second number')
+// if(a > b){
+//     alert(2)
+// } else{
+//     alert(1)
+// }
+
+
+
+// if 8
+// let a = +prompt('Enter first number')
+// let b = +prompt('Enter second number')
+// if(a > b){
+//     alert(a)
+//     alert(b)
+// } else{
+//     alert(b)
+//     alert(a)
+// }
+
+
+
+// if 9
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// let c
+// let d
+// if(a > b){
+//     c = b
+//     d = a
+//     a = c
+//     b = d
+//     alert("A is" + a)
+//     alert('B is' + b)
+// } else{
+//     alert("A is" + a)
+//     alert('B is' + b)
+// }
+
+
+
+// if 10
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// let c
+// let d
+// if(a !=  b){
+//     c = a
+//     d = b
+//     a = a + d
+//     b = b + c
+//     alert('A is ' + a)
+//     alert('B is ' + b)
+// } else{
+//     a = 0
+//     b = 0
+//     alert('A is ' + a)
+//     alert('B is ' + b)
+// }
+
+
+
+// if 11
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// if(a != b){
+//     if(a > b){
+//         a = a
+//         b = a
+//         alert('A is ' + a)
+//         alert('B is ' + b)
+//     } else{
+//         a = b
+//         b = b
+//         alert('A is ' + a)
+//         alert('B is ' + b)
+//     }
+// } else{
+//     a = 0
+//     b = 0
+//     alert('A is ' + a)
+//     alert('B is ' + b)
+// }
+
+
+
+// if 12
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// let c = +prompt('Enter c')
+// if(a < b && a < c){
+//     alert('Smallest number ' + a)
+// } else if(b < a && b < c){
+//     alert('Smallest number ' + b)
+// } else{
+//     alert('Smallest number ' + c)
+// }
+
+
+
+// if 13
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// let c = +prompt('Enter c')
+// if(a < b && a > c){
+//     alert('Middle num ' + a)
+// }else if(a > b && a < c){
+//     alert('Middle num ' + a)
+// }else if(b < a && b > c){
+//     alert('Middle num ' + b)
+// }else if(b > a && b < c){
+//     alert('Middle num ' + b)
+// }else if(c < a && c > b){
+//     alert('Middle num ' + c)
+// }else{
+//     alert('Middle num ' + c)
+// }
+
+
+
+// if 14
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// let c = +prompt('Enter c')
+// if(a < b && a < c && b > a && b > c){
+//     alert('Small num ' + a)
+//     alert('Biggest num ' + b)
+// }else if(a > b && a > c && b < a && b < c){
+//     alert('Small num ' + b)
+//     alert('Biggest num ' + a)
+// }else if(a < b && a < c && c > a && c > b){
+//     alert('Small num ' + a)
+//     alert('Biggest num ' + c)
+// }else if(a > b && a > c && c < a && c < b){
+//     alert('Small num ' + c)
+//     alert('Biggest num ' + a)
+// }else if (b < a && b < c && c > a && c > b){
+//     alert('Small num ' + b)
+//     alert('Biggest num ' + c)
+// }else if (b > a && b > c && c < a && c < b){
+//     alert('Small num ' + c)
+//     alert('Biggest num ' + b)
+// }
+
+
+
+// if 15
+// let a = +prompt('Enter a')
+// let b = +prompt('Enter b')
+// let c = +prompt('Enter c')
+// if(a < b && a < c){
+//     alert('Sum biggest num ' + (b + c))
+// }else if(b < a && b < c){
+//     alert('Sum biggest num ' + (a + c))
+// }else if(c < a && c < b){
+//     alert('Sum biggest num ' + (b + a))
+// }
